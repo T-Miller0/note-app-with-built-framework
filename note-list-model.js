b52427717974ec113b51a918823313bab2135ebc
tom miller <thomas.j.c.miller@outlook.com>
2018-10-16 14:50:09 +0100

    refactored list into its own file

---
+++ note-list-model.js
@@ -0,0 +1,15 @@
+(function(exports) {
+  function NoteList() {
+  this.all = []
+};
+
+  NoteList.prototype.viewAll = function () {
+    return this.all;
+  };
+
+NoteList.prototype.create = function (text) {
+  var note = new Note(text);
+  return this.all.push(text);
+};
+  exports.NoteList = NoteList;
+})(this);
