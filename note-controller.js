4c98b07357ce35aa448aa65414297fe95ee21033
tom miller <thomas.j.c.miller@outlook.com>
2018-11-02 10:04:15 +0000

    added a create method

--- note-controller.js
+++ note-controller.js
@@ -1,19 +1,22 @@
 (function (exports) {
-  function NoteController(noteList = new NoteList()) {
+  function NoteController(noteList = new NoteList(), noteView = NoteListView) {
     noteList.create("Favourite drink: seltzer")
-    this.noteListView = new NoteListView(noteList)
+    this.noteListView = new noteView(noteList)
   }
   NoteController.prototype.insertNoteIntoHtml = function() {
     var element = document.getElementById('app')
     element.innerHTML = this.noteListView.htmlStringify();
   };
 
+
+
 exports.NoteController = NoteController;
-})(this)
+})(this);
 
 
 
-
+var noteController = new NoteController
+noteController.insertNoteIntoHtml()
 // var element = document.getElementById('app')
 // element.innerHTML = "howdy"
 // console.log(element)
