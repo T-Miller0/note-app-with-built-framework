cd99d4e6dbca960aa4e7d7f6edc020e6da3e43d6
tom miller <thomas.j.c.miller@outlook.com>
2018-11-02 10:01:24 +0000

    moved isString and isArray into the same file

--- testFramework/assert.js
+++ testFramework/assert.js
@@ -3,7 +3,7 @@ var assert = {
     if (!assertionToCheck) {
       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy")
     }
-    console.log("%c Test passed! Well Done!")
+    console.log("%c Test passed! Its the same, Well Done!", "color:green")
   },
 
   isString: function (string) {
@@ -11,6 +11,14 @@ var assert = {
       throw new Error(string + ", Should be a String!");
     } else {
       console.log("%c Test passed! It is a String!", "color:green");
+    }
+  },
+
+  isArray: function (array) {
+    if(!typeof array === typeof []) {
+      throw new Error("Should be an array")
+    } else {
+      console.log ("%c Test passed! Is an array", "color:green")
     }
   }
 }
